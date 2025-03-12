fd165d28e323f4d2652250f326c31c0b9366b207
anubhavsinghal12 <anubhavsinghal728@gmail.com>
2025-03-12 15:05:49 +0530

    Add

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://food1User:strongPassword123@127.0.0.1:27017/food1?authSource=food1",
+      "mongodb://food1User:strongPassword123@192.168.182.41:27017/food1?authSource=food1",
       {
-        useNewUrlParser: true,
+     
         useUnifiedTopology: true,
       }
     );
