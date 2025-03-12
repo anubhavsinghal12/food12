7efdca3991a16be44c8008671bd95f4a2ad9971a
anubhavsinghal12 <anubhavsinghal728@gmail.com>
2025-03-13 00:20:09 +0530

    Fix GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/food12/',  // ✅ Add this line (Replace with your GitHub repo name)
+ // ✅ Add this line (Replace with your GitHub repo name)
   server: {
     proxy: {
       '/api': 'http://localhost:3000',  // This is fine for local development
