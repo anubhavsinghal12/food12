861eb66e9d99e8da7542efd96a2d2d148900de61
anubhavsinghal12 <anubhavsinghal728@gmail.com>
2025-03-17 14:12:24 +0530

    add

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -15,10 +15,10 @@ const Verify = () => {
     const verifyPayment = async () =>{
         const response = await axios.post(url+"/api/order/verify",{success, orderId});
         if(response.data.success){
-            navigate('/myorders');
+            navigate('https://food-del-7hph.onrender.com/myorders');
         }
         else{
-            navigate('/')
+            navigate('https://food-del-7hph.onrender.com/')
         }
     }
 
