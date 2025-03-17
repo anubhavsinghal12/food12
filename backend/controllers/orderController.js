3e23ca0bbf691fdb36208ff42750c5f08a8cadd8
anubhavsinghal12 <anubhavsinghal728@gmail.com>
2025-03-17 21:29:33 +0530

    add

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -42,7 +42,7 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            success_url: `${frontend_url}/myorder`,
+            success_url: `${frontend_url}/myorders`,
             cancel_url: `${frontend_url}/`
         });
 
