304fea5d711f27da9c632e1d34816e0a50c126bb
anubhavsinghal12 <anubhavsinghal728@gmail.com>
2025-03-17 20:01:08 +0530

    add

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import  { useContext, useEffect, useState } from 'react';
 import './Verify.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { StoreContext } from './../../components/context/StoreContext';
