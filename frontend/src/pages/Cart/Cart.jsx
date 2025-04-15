358741752a00f7f33d565d1aa757ba5d16e94537
Anubhav <anubhavsinghal728@gmail.com>
2025-04-16 01:06:15 +0530

    add

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -25,7 +25,7 @@ const Cart = () => {
           if(cartItems[item._id]>0){
             return <div>
               <div className="cart-items-title cart-items-item">
-              <img src={url+'/images/'+item.image} alt="" />
+              <img src={url+'images/'+item.image} alt="" />
               <p>{item.name}</p>
               <p>RS {item.price}</p>
               <p>{cartItems[item._id]}</p>
